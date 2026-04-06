bb90e731df9b43dcaaf835e76969be3f32891b65
JDconsorte <jakedaniel.consorte@gmail.com>
2026-04-06 23:45:51 +0800

    Passenger push

---
+++ Milestone1/src/main/java/cc3/milestone1/Passenger.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cc3.milestone1;
+
+/**
+ *
+ * @author User
+ */
+public class Passenger {
+    
+}
